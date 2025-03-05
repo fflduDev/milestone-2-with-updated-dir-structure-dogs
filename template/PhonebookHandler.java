bfe7e2cf64dbc90e122363d704330375b9a7ff63
MJ <michael.muscara@student.fairfield.edu>
2025-03-05 15:01:13 -0500

    did sortByName

--- template/PhonebookHandler.java
+++ template/PhonebookHandler.java
@@ -13,25 +13,34 @@ import java.util.*;
 
 public class PhonebookHandler implements iPhonebookHander{
 
-	private Map<Contact, List<PhonebookEntry>> phonebook = new HashMap<>();
+	private Map<Contact, List<PhonebookEntry>> phonebook;
 	//constructor may be wrong, just fixed the errors
 	public PhonebookHandler(Map<Contact, List<PhonebookEntry>> phonebook) {
-		this.phonebook = phonebook;
+		this.phonebook = new HashMap<>();
 	}
 
 
 	@Override
 	public List<Contact> sortByName() {
 		// TODO Auto-generated method stub
-		List<Contact> result = new ArrayList<>(phonebook.keySet());
+		List<Contact> contacts = new ArrayList<>(phonebook.keySet());
 
-
-		return result;
+		for (int i = 0; i < contacts.size() - 1; i++) {
+			for (int j = i + 1; j < contacts.size() - 1 - i; j++) {
+				if (contacts.get(j).getName().compareTo(contacts.get(j+1)) > 0) {
+					Contact temp = contacts.get(i);
+					contacts.set(j, contacts.get(j+1));
+					contacts.set(j+1, temp);
+				}
+			}
+		}
+		return contacts;
 	}
 
 	@Override
 	public List<PhonebookEntry> binarySearch(List<Contact> sortedContacts, String name) {
 		// TODO Auto-generated method stub
+		List<PhonebookEntry> result = new ArrayList<>();
 		return null;
 	}
 
