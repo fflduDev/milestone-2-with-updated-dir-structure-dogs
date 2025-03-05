03c59eb33e59e768c1db5373704073cd0b32c8bf
MJ <michael.muscara@student.fairfield.edu>
2025-03-05 14:41:08 -0500

    error

--- template/PhonebookHandler.java
+++ template/PhonebookHandler.java
@@ -12,18 +12,9 @@ import java.util.*;
  */
 
 public class PhonebookHandler implements iPhonebookHander{
-
-	private Map<Contact, List<PhonebookEntry>> phonebook;
-	//constructor may be wrong, just fixed the errors
-	public PhonebookHandler(Map<Contact, List<PhonebookEntry>> phonebook) {
-
-		this.phonebook = new HashMap<>(phonebook);
-	}
 	@Override
 	public List<Contact> sortByName() {
 		// TODO Auto-generated method stub
-		List<Contact> result = new ArrayList<>(phonebook.keySet());
-
 		return null;
 	}
 
@@ -36,14 +27,6 @@ public class PhonebookHandler implements iPhonebookHander{
 	@Override
 	public void display(List<Contact> sortedContacts) {
 		// TODO Auto-generated method stub
-		System.out.println("[");
-		for (int i = 0; i < sortedContacts.size(); i++) {
-			System.out.println(sortedContacts.get(i));
-			if (i != sortedContacts.size() - 1) {
-				System.out.print(", ");
-			}
-		}
-		System.out.println("]");
 	}
 
  
