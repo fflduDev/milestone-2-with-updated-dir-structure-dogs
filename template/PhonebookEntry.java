b3797673fcc896759807ade38938e5637e4870de
JackCallanan <john.callanan1@student.fairfield.edu>
2025-03-05 15:09:50 -0500

    phonebook entry updated

--- template/PhonebookEntry.java
+++ template/PhonebookEntry.java
@@ -9,8 +9,24 @@ package template;
  *   
  */
 
-class PhonebookEntry{
+class PhonebookEntry {
+    private String phoneNumber;
+    private String type;
 
- 
+    public void phonebookEntry(String phoneNumber, String type) {
+        this.phoneNumber = phoneNumber;
+        this.type = type;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return type + ": " + phoneNumber;
+    }
 }
-	
